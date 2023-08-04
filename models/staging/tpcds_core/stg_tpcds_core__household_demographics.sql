9f35cf3db7da9e30291ffab5af9991373cc91132
Jeremy Cohen <jeremy@dbtlabs.com>
2023-08-04 13:57:38 +0200

    Add income band info

--- models/staging/tpcds_core/stg_tpcds_core__household_demographics.sql
+++ models/staging/tpcds_core/stg_tpcds_core__household_demographics.sql
@@ -10,6 +10,7 @@ renamed as (
 
     select
         hd_demo_sk as household_demographics_sk,
+        hd_income_band_sk as income_band_sk,
         hd_buy_potential as buy_potential,
         hd_dep_count as dep_count,
         hd_vehicle_count as vehicle_count
