3a1aa26dc077907085c4ba150cb30e791f401ecf
Dave Connors <dave.connors@fishtownanalytics.com>
2023-08-23 15:41:56 +0000

    add version to stores

---
+++ models/marts/stores_v2.sql
@@ -0,0 +1,26 @@
+with 
+
+stores as (
+    select * from {{ ref('stg_tpcds_core__store') }}
+),
+
+date_dim as (
+    
+    select * from {{ ref('stg_tpcds_core__date_dim') }}
+
+),
+
+final as (
+
+    select 
+        stores.*, 
+        'store' as entity_type, 
+        date_dim.date as store_closed_date
+    
+    from stores
+    left join date_dim
+        on stores.closed_date_sk = date_dim.date_sk
+
+)
+
+select * from final
